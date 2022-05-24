9094b4f90de4b311e8f57e8f1f49e362c49759c0
imbiotech <jiminsub12@naver.com>
2022-05-24 23:41:51 +0900

    2022.05.24 Study

--- Job Scrapper/JOB_Scrapper_F_NOMAD.py
+++ Job Scrapper/JOB_Scrapper_F_NOMAD.py
@@ -85,3 +85,17 @@ def exporter_csv(jobs):
         # Dict 형에서 List 형으로 전환할 때 key만 가져오고 싶으면 Dict.keys() value만 가져오고 싶으면 Dict.values()
         writer.writerow(list(job.values()))
     return
+
+
+# 네이버 블로그 스크래핑
+import requests
+from bs4 import BeautifulSoup
+URL="https://blog.naver.com/네이버아이디/게시글번호"
+result = requests.get(URL)
+soup = BeautifulSoup(result.text, "html.parser")
+URL_blog = "https://blog.naver.com/"+soup.find("iframe")["src"]
+print(URL_blog)
+result2 = requests.get(URL_blog)
+soup2 = BeautifulSoup(result2.text, "html.parser")
+data = soup2.find("div", {"class": "se-main-container"})
+print(data.text)
