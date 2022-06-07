91b429109912ad45e45ed0684afc1b2a1621de08
imbiotech <jiminsub12@naver.com>
2022-06-07 21:54:19 +0900

    2022.06.07 Study

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,18 +21,40 @@
 
 
 // html의 validation 및 제한, 제출 기능을 사용하면 JS에서 지정할 필요가 없음
-const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
+
 
 // arg를 안 주면 code가 실행되지 않음
 function onLISubmit (event) {
     // Submit에 대해서 default event 발생을 방지하는 방법
     event.preventDefault();
-    console.log(loginInput.value);
+    
+    // console.log(loginInput.value);
     // const name = loginInput.value;
     // console.log(name);
     // console.log(event);
+
+    // submit 된 username info를 가져옴
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`; // `를 활용하면 python의 f"" 처럼 사용할 수 있음"
+
 };
 
-loginForm.addEventListener("submit", onLISubmit);
+// link 클릭에 대한 함수 제한 방법
+function handleLinkClick (event) {
+    // alert로 인해서 순간적으로 정지됨.
+    // alert("clicked!");
+    event.preventDefault();
+};
 
+
+loginForm.addEventListener("submit", onLISubmit);
+// link.addEventListener("click",handleLinkClick)
