a2c2a706649830ae16c9df4b85593748542f79e7
imbiotech <jiminsub12@naver.com>
2022-06-16 01:14:32 +0900

    2022.06.16 Study

--- JavaScript/cloneChrome/JSs/backgrounds.js
+++ JavaScript/cloneChrome/JSs/backgrounds.js
@@ -3,7 +3,8 @@
 const images = [
     "0.jpg",
     "1.jpg",
-    "2.jpg"
+    "2.jpg",
+    "3.jpg"
 ];
 let indexing_images = Math.floor(Math.random() * images.length);
 let imageOfToday = images[indexing_images];
