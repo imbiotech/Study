ff11179359cd89e6441724f6419c590445ba08e8
imbiotech <jiminsub12@naver.com>
2022-07-04 13:03:30 +0900

    2022.07.04 Study

--- Binance API/binance.py
+++ Binance API/binance.py
@@ -1,22 +1,20 @@
 from function import *
-import asyncio
-import pandas as pd
 from binance_lib.streams import *
 
-client = asyncio.run(on_client())
-
-# 시장가 매매 (side, quantity)
-# market(side,quantity)
-
-# 지정가 매매 (side,price,quantity)
-# limit(side,price,quantity)
-
-# TP, SL, TPL, SLL (side, quantity, stop_type, price)
-# profit_n_loss(PL, side, quantity, stop_type="Delta 500",price=0)
-
-
-
-
+asyncio.run(on_client())
+# wallets, data = asyncio.run(on_client())
+#
+# count=0
+# while count<10:
+#     new_wallets = getWallet
+#     new_data = asyncio.run(getData())
+#     if wallets != new_wallets:
+#         wallets = new_wallets
+#         print("지갑 업데이트")
+#     if data != new_data:
+#         data = new_data
+#         market("buy", 0.0005)
+#     count+=1
 
 # account_info = r_client.get_account_information() # Future 서버 전용
 # symbol = r_client.get_symbol_price_ticker(symbol=SYMBOL)
@@ -50,22 +48,6 @@ ORDER_STATUS_PENDING_CANCEL = 'PENDING_CANCEL'
 ORDER_STATUS_REJECTED = 'REJECTED'
 ORDER_STATUS_EXPIRED = 'EXPIRED'
 
-KLINE_INTERVAL_1MINUTE = '1m'
-KLINE_INTERVAL_3MINUTE = '3m'
-KLINE_INTERVAL_5MINUTE = '5m'
-KLINE_INTERVAL_15MINUTE = '15m'
-KLINE_INTERVAL_30MINUTE = '30m'
-KLINE_INTERVAL_1HOUR = '1h'
-KLINE_INTERVAL_2HOUR = '2h'
-KLINE_INTERVAL_4HOUR = '4h'
-KLINE_INTERVAL_6HOUR = '6h'
-KLINE_INTERVAL_8HOUR = '8h'
-KLINE_INTERVAL_12HOUR = '12h'
-KLINE_INTERVAL_1DAY = '1d'
-KLINE_INTERVAL_3DAY = '3d'
-KLINE_INTERVAL_1WEEK = '1w'
-KLINE_INTERVAL_1MONTH = '1M'
-
 ORDER_RESP_TYPE_ACK = 'ACK'
 ORDER_RESP_TYPE_RESULT = 'RESULT'
 ORDER_RESP_TYPE_FULL = 'FULL'
@@ -79,3 +61,7 @@ AGG_LAST_TRADE_ID  = 'l'
 AGG_TIME           = 'T'
 AGG_BUYER_MAKES    = 'm'
 AGG_BEST_MATCH     = 'M'
+
+
+
+
